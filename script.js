da97cc01f243898df25908209e27d4029af6d9c1
okliland-kodeloftet <okliland@hotmail.com>
2026-04-14 10:48:47 +0200

    else

--- script.js
+++ script.js
@@ -17,3 +17,37 @@ hamburgerIcon.addEventListener("click", function () {
 //
 
 /* Undervisning 16: JavaScript Conditions & Operators */
+
+
+let alder = 18;
+
+if(alder >=65) {
+  console.log("Du er for gammel")
+}
+
+else if(alder >= 18) {
+  console.log("Du er gammel nok.")
+}
+
+else{
+  console.log("Du er ikke gammel nok")
+}
+
+
+let rolle = "admin";
+
+if(rolle = "admin"){
+  console.log("Admin bruker. Full rettigheter over systemet")
+}
+
+else if (rolle == "bruker"){
+  console.log("Normal konto type, ikke alle rettigheter")
+}
+
+  else if(rolle == "gjest"){
+    console.log("gjestebruker Begrensede rettigheter")
+  }
+
+  else {
+    console.log("Ugyldig bruker. Blokker gateway")
+  }
