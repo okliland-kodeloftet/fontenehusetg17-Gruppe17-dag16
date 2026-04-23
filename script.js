c8b25bf4eb6a60bad07c7f98fe4f85d0b48c2a6f
okliland-kodeloftet <okliland@hotmail.com>
2026-04-23 12:02:03 +0200

    kkd

--- script.js
+++ script.js
@@ -598,14 +598,22 @@ console.log("Oppgave 6");
 /* Bonus oppgave
 Opprett en liste "users" med tre objekter. Dem skal hver ha et navn og alder. Ved bruk av .map, returner en ny array som inkluderer kun navnene dems. */
 
+console.assert.og("bonus oppgave")
+
 const users = [
   {names: "nils", ages: 22},
   {names: "olaf", ages: 33},
   {names: "gaute", ages: 23},
-];{
+];
 
-const justNames = users.map(functions(includes(names))
+
+
+const justNames = users.map(functions(user)
+{
+  return users.justNames;
+}
 )
 console.log(justNames)
-};
+;
+
 
